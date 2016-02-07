3d0d1d202cb83c77a83a9f6c9c937ab6ab18cc4d
yshkrk1011 <yshkrk1011@gmail.com>
2016-02-07 15:15:28 +0900

    up

--- wheatherapi/20160207/createjs/js/bitmap.js
+++ wheatherapi/20160207/createjs/js/bitmap.js
@@ -10,7 +10,6 @@ $(function(){
 ​
   var stage = new createjs.Stage('canvasEl');
 
-  //loadQueue ローディング
   var loadQueue = new createjs.loadQueue();
   loadQueue.loadManifest(manifest); //読み込み開始
 
@@ -19,15 +18,13 @@ $(function(){
   });
 
 ​
-​
-  // フレームレート
+
   createjs.Ticker.setFPS(30);
   createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
 ​
-​
-  // アニメーションの設定
+
   createjs.Ticker.addEventListener('tick',function(){
-    // ステージの更新
+
     stage.update();
   });
 ​
