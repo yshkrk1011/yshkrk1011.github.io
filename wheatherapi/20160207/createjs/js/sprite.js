cf818100c02d7deefe600aaa5134db7f79038a00
yshkrk1011 <yshkrk1011@gmail.com>
2016-02-07 16:31:46 +0900

    up

--- wheatherapi/20160207/createjs/js/sprite.js
+++ wheatherapi/20160207/createjs/js/sprite.js
@@ -16,31 +16,11 @@ var stage = new createjs.Stage('canvasEl');
   });
 
   function showVage(){
-    var caSprite = new createjs.SpriteSheet(calciumSprite);
-    var calcium = new createjs.Sprite(caSprite,'stand');
-    stage.addChild(calcium);
-    vitaminC.x = 150;
-    vitaminC.y = 150;
-    vitaminC.alpha = 0;
-
-    createjs.Tween.get(calcium).to({alpha:1},500).wait(1000).call(calciumWalk).to({x:800},4000).call(calciumStand);
-
-
-    //ビタミンCが歩く
-    function calciumWalk(){
-      calcium.gotoAndPlay('walk');
-    }
-
-    //ビタミンCが止まる
-    function calciumStand(){
-      calcium.gotoAndPlay('stand');
-    }
-
     var vcSprite = new createjs.SpriteSheet(vitaminCSprite);
     var vitaminC = new createjs.Sprite(vcSprite,'stand');
     stage.addChild(vitaminC);
-    calcium.x = 300;
-    calcium.y = 300;
+    vitaminC.x = 150;
+    vitaminC.y = 150;
     vitaminC.alpha = 0;
 
     createjs.Tween.get(vitaminC).to({alpha:1},500).wait(1000).call(vitaminCWalk).to({x:500},4000).call(vitaminCStand);
@@ -55,6 +35,29 @@ var stage = new createjs.Stage('canvasEl');
     function vitaminCStand(){
       vitaminC.gotoAndPlay('stand');
     }
+
+    //calcium
+    function showVage(){
+      var caSprite = new createjs.SpriteSheet(calciumSprite);
+      var calcium = new createjs.Sprite(caSprite,'stand');
+      stage.addChild(calcium);
+      calcium.x = 300;
+      calcium.y = 300;
+      calcium.alpha = 0;
+
+      createjs.Tween.get(calcium).to({alpha:1},500).wait(1000).call(calciumWalk).to({x:500},4000).call(calciumStand);
+
+
+      //ビタミンCが歩く
+      function calciumWalk(){
+        calcium.gotoAndPlay('walk');
+      }
+
+      //ビタミンCが止まる
+      function calciumStand(){
+        calcium.gotoAndPlay('stand');
+      }
+
 
     stage.update();
   };
