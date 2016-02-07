a0a397ec985acf55eac12c054356deff27ea66ca
yshkrk1011 <yshkrk1011@gmail.com>
2016-02-07 15:07:09 +0900

    up

---
+++ wheatherapi/20160207/createjs/js/bitmap.js
@@ -0,0 +1,36 @@
+//先読みするリスト
+var manifest = [
+  {src:'img/tomato.png'},
+  {src:'img/pumpkin.png'},
+  {src:'img/greenPepper.png'}
+];
+
+
+// DOMを読み込んだら
+$(function(){
+​
+  // ステージ（canvas）の設定
+  var stage = new createjs.Stage('canvasEl');
+
+  //loadQueue ローディング
+  var loadQueue = new createjs.loadQueue();
+  loadQueue.loadManifest(manifest); //読み込み開始
+
+  loadQueue.addEventListener('complete',function(){
+    console.log('読み込み完了！！！！');
+  });
+
+​
+​
+  // フレームレート
+  createjs.Ticker.setFPS(30);
+  createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
+​
+​
+  // アニメーションの設定
+  createjs.Ticker.addEventListener('tick',function(){
+    // ステージの更新
+    stage.update();
+  });
+​
+});
