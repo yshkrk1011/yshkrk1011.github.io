c5ec36c251e25c2e910052e31af6944ad5523601
yshkrk1011 <yshkrk1011@gmail.com>
2016-02-07 16:41:35 +0900

    up

--- wheatherapi/20160207/createjs/js/sprite.js
+++ wheatherapi/20160207/createjs/js/sprite.js
@@ -15,11 +15,6 @@ var stage = new createjs.Stage('canvasEl');
     showVage();
   });
 
-  loadQueue.addEventListener('complete',function(){
-    console.log('読み込み完了！！！！');
-    showVage2();
-  });
-
   function showVage(){
     var vcSprite = new createjs.SpriteSheet(vitaminCSprite);
     var vitaminC = new createjs.Sprite(vcSprite,'stand');
@@ -41,27 +36,6 @@ var stage = new createjs.Stage('canvasEl');
       vitaminC.gotoAndPlay('stand');
     }
 
-    //calcium
-    function showVage2(){
-      var caSprite = new createjs.SpriteSheet(calciumSprite);
-      var calcium = new createjs.Sprite(caSprite,'stand');
-      stage.addChild(calcium);
-      calcium.x = 300;
-      calcium.y = 300;
-      calcium.alpha = 0;
-
-      createjs.Tween.get(calcium).to({alpha:1},500).wait(1000).call(calciumWalk).to({x:500},4000).call(calciumStand);
-
-
-      //ビタミンCが歩く
-      function calciumWalk(){
-        calcium.gotoAndPlay('walk');
-      }
-
-      //ビタミンCが止まる
-      function calciumStand(){
-        calcium.gotoAndPlay('stand');
-      }
 
 
     stage.update();
