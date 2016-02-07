ba70429db488b567594eab21c41539a0eec71d83
yshkrk1011 <yshkrk1011@gmail.com>
2016-02-07 14:52:28 +0900

    up

--- wheatherapi/20160207/createjs/js/01.js
+++ wheatherapi/20160207/createjs/js/01.js
@@ -1,17 +1,43 @@
+
+// DOMを読み込んだら
 $(function(){
-
-  //stageの設定
+​
+  // ステージ（canvas）の設定
   var stage = new createjs.Stage('canvasEl');
-
-  //円
+​
+  // 円 drawCircle(中心のx座標,中心のy座標,直径)
   var ball = new createjs.Shape();
   ball.graphics.beginFill('#0000FF').drawCircle(0,0,40);
-  ball.x = 200;
-  ball.y = 200;
+  ball.x = 100;
+  ball.y = 100;
+  ball.alpha = 0;
   stage.addChild(ball);
-
-  //ステージの更新
-  stage.update();
-
-
+​
+  // 四角形　drawRect(x座標,y座標,横幅,縦幅)
+  var box = new createjs.Shape();
+  box.graphics.beginFill('#FF0000').drawRect(0,0,50,50);
+  box.x = 100;
+  box.y = 200;
+  stage.addChild(box);
+​
+​
+  box.addEventListener('click',function(){
+    // アニメーション
+    createjs.Tween.get(ball).to({alpha:1},1000).wait(1000).to({x:500}, 1000, createjs.Ease.bounceOut);
+​
+​
+  });
+​
+​
+  // フレームレート
+  createjs.Ticker.setFPS(30);
+  createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
+​
+​
+  // アニメーションの設定
+  createjs.Ticker.addEventListener('tick',function(){
+    // ステージの更新
+    stage.update();
+  });
+​
 });
